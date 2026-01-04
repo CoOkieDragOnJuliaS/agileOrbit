9fed42c35770bd7350948f5e364d36179d46f0cc
Julia S <julia.sass@stud.fh-campuswien.ac.at>
2026-01-04 15:08:41 +0100

    [Julia] refactoring: Deleting unused import, after refactor and bugfixes

--- client/src/components/client/KanbanBoard.js
+++ client/src/components/client/KanbanBoard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import './KanbanBoard.css';
 import TaskModal from './TaskModal';
 
