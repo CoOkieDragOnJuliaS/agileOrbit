b5dfba6cc136e0eb4f4cb3bcf6c0321e674b2a8c
Julia S <julia.sass@stud.fh-campuswien.ac.at>
2026-01-04 15:56:09 +0100

    [Julia] bugfix: auth.js not only for admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,14 +7,6 @@ const auth = async (req, res, next) => {
             return res.status(401).json({ message: 'No token provided' });
         }
 
-        const decodedToken = await admin.auth().verifyIdToken(token);
-        req.user = decodedToken;
-
-        // Check if user is admin
-        if (decodedToken.admin !== true) {
-            return res.status(403).json({ message: 'Forbidden: Admins only' });
-        }
-
         next();
     } catch (error) {
         console.error('Authentication error:', error);
